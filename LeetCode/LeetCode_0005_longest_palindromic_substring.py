977b50759afcc121a917b25ce82e29d88b2a34e1
Liu Kun <liukunup@163.com>
2022-11-21 23:47:32 +0800

    第5题 最长回文子串 Manacher解法

--- LeetCode/LeetCode_0005_longest_palindromic_substring.py
+++ LeetCode/LeetCode_0005_longest_palindromic_substring.py
@@ -101,6 +101,35 @@ class Solution:
                 start, end = left2, right2
         return s[start: end + 1]
 
+    def expandAroundCenter_v3(self, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return (right - left - 2) // 2
+
+    def longestPalindrome_v3(self, s: str) -> str:
+        start, end = 0, -1
+        s = "#" + "#".join(list(s)) + "#"
+        # 臂长
+        arm_len = []
+        right = -1
+        j = -1
+        for i in range(len(s)):
+            if right >= i:
+                i_sym = 2 * j - i
+                min_arm_len = min(arm_len[i_sym], right - i)
+                cur_arm_len = self.expandAroundCenter_v3(s, i - min_arm_len, i + min_arm_len)
+            else:
+                cur_arm_len = self.expandAroundCenter_v3(s, i, i)
+            arm_len.append(cur_arm_len)
+            if i + cur_arm_len > right:
+                j = i
+                right = i + cur_arm_len
+            if 2 * cur_arm_len + 1 > end - start:
+                start = i - cur_arm_len
+                end = i + cur_arm_len
+        return s[start + 1:end + 1:2]
+
 
 # 请完成以下测试代码
 class SolutionTest(unittest.TestCase):
@@ -114,10 +143,10 @@ class SolutionTest(unittest.TestCase):
 
     # 请设计一些测试用例来验证
     def test_1(self):
-        self.assertEqual(self.inst.longestPalindrome_v2("babad"), "bab")
+        self.assertIn(self.inst.longestPalindrome_v3("babad"), ["bab", "aba"])
 
     def test_2(self):
-        self.assertEqual(self.inst.longestPalindrome_v2("cbbd"), "bb")
+        self.assertEqual(self.inst.longestPalindrome_v3("cbbd"), "bb")
 
 
 if __name__ == "__main__":
