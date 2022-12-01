db24cc428e7a04466685ee3ebf4c3ed0669851d2
billykunliu <billykunliu@tencent.com>
2022-12-01 20:42:21 +0800

    第53题 最大子数组和 动态规划、分治

--- LeetCode/LeetCode_0053_maximum_subarray.py
+++ LeetCode/LeetCode_0053_maximum_subarray.py
@@ -61,12 +61,49 @@ from typing import List
 
 # 请完成以下开发代码(如不符合语法要求,自行修改即可)
 class Solution:
+
     def maxSubArray(self, nums: List[int]) -> int:
         n = len(nums)
         for i in range(1, n):
             if nums[i - 1] > 0:
                 nums[i] += nums[i - 1]
         return max(nums)
+
+    def maxSubArray_v2(self, nums: List[int]) -> int:
+        size = len(nums)
+        if size == 0:
+            return 0
+        return self.__max_sub_array(nums, 0, size - 1)
+
+    def __max_sub_array(self, nums, left, right):
+        if left == right:
+            return nums[left]
+        mid = (left + right) >> 1
+        return max(self.__max_sub_array(nums, left, mid),
+                   self.__max_sub_array(nums, mid + 1, right),
+                   self.__max_cross_array(nums, left, mid, right))
+
+
+    def __max_cross_array(self, nums, left, mid, right):
+        # 一定包含 nums[mid] 元素的最大连续子数组的和，
+        # 思路是看看左边"扩散到底"，得到一个最大数，右边"扩散到底"得到一个最大数
+        # 然后再加上中间数
+        left_sum_max = 0
+        start_left = mid - 1
+        s1 = 0
+        while start_left >= left:
+            s1 += nums[start_left]
+            left_sum_max = max(left_sum_max, s1)
+            start_left -= 1
+
+        right_sum_max = 0
+        start_right = mid + 1
+        s2 = 0
+        while start_right <= right:
+            s2 += nums[start_right]
+            right_sum_max = max(right_sum_max, s2)
+            start_right += 1
+        return left_sum_max + nums[mid] + right_sum_max
 
 
 # 请完成以下测试代码
